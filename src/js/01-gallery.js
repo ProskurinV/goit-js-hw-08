33612b19ae2b89a1444d560c3e1837db0ff88634
ProskurinV <proskyrinv.g@gmail.com>
2022-08-01 21:55:03 +0300

    task2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,6 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-import SimpleLightbox from 'simplelightbox';
-// Описан в документации
+
 import SimpleLightbox from 'simplelightbox';
 // Дополнительный импорт стилей
 import 'simplelightbox/dist/simple-lightbox.min.css';
