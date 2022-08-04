02d16b4ceae9dee29f42c9e1c6e14a46041c7c8a
ProskurinV <proskyrinv.g@gmail.com>
2022-08-04 23:15:58 +0300

    task3inPros

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(event) {
+  const formEl = event.currentTarget.elements;
+  const email = formEl.email.value;
+  const message = formEl.message.value;
+
+  formData = { email, message };
+
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formEl = event.currentTarget.elements;
+  const email = formEl.email.value;
+  const message = formEl.message.value;
+
+  formData = { email, message };
+  console.log(formData);
+  localStorage.getItem(LOCALSTORAGE_KEY, JSON.parse(formData));
+  // const formData = new FormData(event.currentTarget);
+  // formData.forEach((value, name) => {
+  //   console.log(name, value);
+  // });
+
+  localStorage.clear();
+  form.reset();
+}
